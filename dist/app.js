ff3417c434eba548a2cdda4add2270976030f96c
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2024-06-03 00:32:02 -0400

    v2

--- dist/app.js
+++ dist/app.js
@@ -589,9 +589,9 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 var jQueryScript = document.createElement("script");
 jQueryScript.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
 jQueryScript.onload = function() {
-    var jQuery = $.noConflict(true);
+    var jQuery = window.jQuery.noConflict(true);
     // Encapsulate jQuery code in an anonymous function
-    (function($1) {
+    (function($) {
         // Ensure Webflow array exists
         window.Webflow = window.Webflow || [];
         // Push function to Webflow array
@@ -599,7 +599,7 @@ jQueryScript.onload = function() {
             // Function to toggle dark-light mode
             function toggleDarkLightMode() {
                 var darkMode = localStorage.getItem("darkMode") === "true";
-                var body = $1("body");
+                var body = $("body");
                 var textColor = darkMode ? "#ffffff" : "#000000";
                 var textColorBrand = darkMode ? "#d5ef00" : "#000000";
                 var bgColor = darkMode ? "#061414" : "#ffffff";
@@ -608,36 +608,36 @@ jQueryScript.onload = function() {
                     "color": textColor
                 });
                 // Set styles for various elements
-                $1(".paragraph, .rich-text p, .w-pagination-previous, .w-pagination-next, .empath-text, .footer-link, .about-title, .title, .h2, .course-labels, .bonus-h2, .true-or-false-text, .slider-arrow-icon").css("color", textColor);
-                $1(".empath-text-brand, .learn-more-link").css("color", textColorBrand);
-                $1(".about-info-wrap").css("background-color", bgColor);
-                $1(".footer-logo-image").attr("src", darkMode ? "https://uploads-ssl.webflow.com/65f0d5739b651eae06b2ca56/662831673fd2dec6eba4fecb_FST%20Logo-dark.png" : "https://uploads-ssl.webflow.com/65f0d5739b651eae06b2ca56/6622cffa219b956a49edd98c_FST%20Logo-footer.png");
-                $1(".arrow-learn-more").attr("src", darkMode ? "https://uploads-ssl.webflow.com/65f0d5739b651eae06b2ca56/665d430ea891207010a463e6_arrow-dark.svg" : "https://uploads-ssl.webflow.com/65f0d5739b651eae06b2ca56/66302f64ff07464d678857b8_arrow-learn-more.svg");
+                $(".paragraph, .rich-text p, .w-pagination-previous, .w-pagination-next, .empath-text, .footer-link, .about-title, .title, .h2, .course-labels, .bonus-h2, .true-or-false-text, .slider-arrow-icon").css("color", textColor);
+                $(".empath-text-brand, .learn-more-link").css("color", textColorBrand);
+                $(".about-info-wrap").css("background-color", bgColor);
+                $(".footer-logo-image").attr("src", darkMode ? "https://uploads-ssl.webflow.com/65f0d5739b651eae06b2ca56/662831673fd2dec6eba4fecb_FST%20Logo-dark.png" : "https://uploads-ssl.webflow.com/65f0d5739b651eae06b2ca56/6622cffa219b956a49edd98c_FST%20Logo-footer.png");
+                $(".arrow-learn-more").attr("src", darkMode ? "https://uploads-ssl.webflow.com/65f0d5739b651eae06b2ca56/665d430ea891207010a463e6_arrow-dark.svg" : "https://uploads-ssl.webflow.com/65f0d5739b651eae06b2ca56/66302f64ff07464d678857b8_arrow-learn-more.svg");
                 // Add/remove classes for dark mode
-                $1(".dark-light-button, .icon-circle-dark").toggleClass("opaque-dark", darkMode);
-                $1(".icon-circle-light").toggleClass("opaque-light", darkMode);
+                $(".dark-light-button, .icon-circle-dark").toggleClass("opaque-dark", darkMode);
+                $(".icon-circle-light").toggleClass("opaque-light", darkMode);
                 if (darkMode) {
-                    $1(".hero-image-wrap-light").hide();
-                    $1(".hero-image-wrap-dark").show();
+                    $(".hero-image-wrap-light").hide();
+                    $(".hero-image-wrap-dark").show();
                 } else {
-                    $1(".hero-image-wrap-light").show();
-                    $1(".hero-image-wrap-dark").hide();
+                    $(".hero-image-wrap-light").show();
+                    $(".hero-image-wrap-dark").hide();
                 }
                 // Update local storage
-                localStorage.setItem("we_clicks", darkMode);
+                localStorage.setItem("darkMode", darkMode);
             }
             // Check and toggle dark-light mode on page load
-            $1(document).ready(function() {
+            $(document).ready(function() {
                 toggleDarkLightMode();
             });
             // Toggle dark-light mode on button click
-            $1(".dark-light-button").click(function() {
+            $(".dark-light-button").click(function() {
                 var darkMode = localStorage.getItem("darkMode") === "true";
                 localStorage.setItem("darkMode", !darkMode);
                 toggleDarkLightMode();
             });
             // Store dark-light mode selection in local storage when navigating to a different page
-            $1(window).on("beforeunload", function() {
+            $(window).on("beforeunload", function() {
                 var darkMode = localStorage.getItem("darkMode") === "true";
                 localStorage.setItem("darkMode", darkMode);
             });
